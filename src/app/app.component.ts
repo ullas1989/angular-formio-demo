6acd2702077c4d28d29d5884b9897be4b7c627aa
Ullas Kumar <ukumar@olsi.net>
2020-03-19 12:35:54 -0500

    inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  @ViewChild('json') jsonElement?: ElementRef;
+  public formRenderer: any;
+  public form: any = {
+    components: []
+  };
+
+  onChange(event) {
+    this.jsonElement.nativeElement.innerHTML = '';
+    this.jsonElement.nativeElement.appendChild(document.createTextNode(JSON.stringify(event.form, null, 4)));
+    // this.formRenderer = event.form;
+  }
+}
