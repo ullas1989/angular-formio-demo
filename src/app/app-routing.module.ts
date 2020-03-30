bec94f2a68fda6745e51505890196866f0d51bc4
Ullas Kumar <hyd.ullaskumar@gmail.com>
2020-03-30 16:37:37 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { BuilderComponent } from './builder/builder/builder.component';
 import { RenderComponent } from './renderer/render/render.component';
 
 const routes = [
-  { path: '', redirectTo: 'builder', pathMatch: 'full' },
+  { path: '', redirectTo: 'render', pathMatch: 'full' },
   {
     path: 'builder',
     component: BuilderComponent
